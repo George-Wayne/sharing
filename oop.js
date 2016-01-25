26f9230b7b4388ddd44a52e55109d44c31252b1e
George <george01.wang@vipshop.com>
2016-01-25 22:55:11 +0800

    aadd call() and apply()

--- oop.js
+++ oop.js
@@ -35,7 +35,7 @@
  *
  **/
 
-/*工厂方式*/
+/*************** 1、工厂方式 ***************/
 function createCar(Color, Doors, Money) {
     var Car = new Object;//第一步：创建对象
     Car.color = Color;//第二步：给对象添加属性和属性值
@@ -55,7 +55,7 @@ Car1.showColor();
 Car2.showColor();
 
 
-/*构造函数方式*/
+/*************** 2、构造函数方式 ***************/
 function Student(name, age) {//第一步：构造函数名首字母大写
     this.name = name;//第二步：用this声明成员变量
     this.age = age;
@@ -70,7 +70,7 @@ console.log('stu1.name', stu1.name);
 stu2.showName();
 
 
-/*prototype原型方式*/
+/*************** 2、prototype函数原型方式 ***************/
 function Book(name, price) {//第一步：必须是构造函数
     this.name = name;
     this.price = price;
@@ -91,3 +91,27 @@ var his = new History();
 History.prototype.color = 'yellow';
 his.outPrice();
 console.log('his', his.color, his.name, his.price, his.size);
+
+
+/*************** 4、call() 和 apply()方式 ***************/
+function Skye(age, gender) {
+    this.age = age;
+    this.gender = gender;
+    this.profile = function () {
+        console.log('Age is ' + this.age + ' and ' + 'gender is ' + this.gender);
+    };
+}
+
+function George(age, gender) {
+    Skye.call(this, age, gender);//call的方式：this指当前所在的对象，参数以列表的形式传入。
+}
+var george01 = new George(25, 'male');
+george01.profile();
+console.log('george01', george01.age, george01.gender);
+
+function Wayne(age, gender) {
+    Skye.apply(this, [age, gender]);//apply的方式：this指当前所在的对象，参数以数组的形式传入。
+}
+var wayne01 = new Wayne(30, 'female');
+wayne01.profile();
+console.log('wayne01', wayne01.age, wayne01.gender);
